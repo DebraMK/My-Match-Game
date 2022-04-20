d27afb3e89b6492e6bd79fcb883c06e0241ea296
DebraMK <dmkochrn@outlook.com>
2022-04-19 21:35:15 -0500

    initial commit very basic

---
+++ script.js
@@ -0,0 +1,18 @@
+function flipCard(){
+    // let element = card(put asset here?)
+    // addEventListener = when player clicks on card, it flips and displays a symbol
+    // if/else statement?  if 2 cards flipped and match, then pair up and move to discard.
+    // display "You got a match!"
+    // else display "No match."
+    // cards flip back over. 
+}
+
+function countMatches(){
+    // when a match is made +1 to count.
+    // need disply for count
+}
+
+function timer(){
+    // when 1st card flipped, start timer. 
+    // stop timer when all cards matched.
+}
