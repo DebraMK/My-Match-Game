0370fc13f9b9a997696043a055354e4ac9625fc1
DebraMK <dmkochrn@outlook.com>
2022-04-23 11:04:40 -0500

    mostly assets added

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function flipCard(){
-    // let element = card(put asset here?)
+// const image_input = document.querySelector("#image_input");
+// var uploaded_image = "";
+// image_input.addEventListener("change".function(){
+//     const reader = new File();
+//     reader.addEventListener("load", () => {
+//         uploaded_image = reader.result;
+//         document.querySelector("display_image").style.backgroundImage = 'url($(uploaded_image))';
+//     })
+//     reader.readAsDataURL(this.files[0]);
+// })
+// set up cards with 2 same shapes under 2 random cards, 12 shapes for 24 cards.
+
+
+const cardInner = document.getElementsByClassName("cardInner"); 
     // addEventListener = when player clicks on card, it flips and displays a symbol
-    // if/else statement?  if 2 cards flipped and match, then pair up and move to discard.
-    // display "You got a match!"
-    // else display "No match."
-    // cards flip back over. 
+cardInner.addEventListener("click", flipCard);  
+
+function flipCard(){
+    card.classList.toggle("cardInner");
 }
+
+// flipCard()
 
 function countMatches(){
     // when a match is made +1 to count.
@@ -15,4 +29,5 @@ function countMatches(){
 function timer(){
     // when 1st card flipped, start timer. 
     // stop timer when all cards matched.
+
 }
