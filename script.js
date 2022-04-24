ebe7151ec1971e103c6a4f033fcb635ecb270aa9
DebraMK <dmkochrn@outlook.com>
2022-04-24 13:31:41 -0500

    html and css redone

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-// const image_input = document.querySelector("#image_input");
-// var uploaded_image = "";
-// image_input.addEventListener("change".function(){
-//     const reader = new File();
-//     reader.addEventListener("load", () => {
-//         uploaded_image = reader.result;
-//         document.querySelector("display_image").style.backgroundImage = 'url($(uploaded_image))';
-//     })
-//     reader.readAsDataURL(this.files[0]);
-// })
-// set up cards with 2 same shapes under 2 random cards, 12 shapes for 24 cards.
+// shuffle board
+// flip 2 cards and check for match
+// if match, keep face up, else unflip
+// flip only 2 cards at a time  and no double clicks
+// timer start on 1st clip, stop when all cards matched
+// reset game or new game button
 
 
 const cardInner = document.getElementsByClassName("cardInner"); 
     // addEventListener = when player clicks on card, it flips and displays a symbol
-cardInner.addEventListener("click", flipCard);  
+// cardInner.addEventListener("click", flipCard);  
 
-function flipCard(){
-    card.classList.toggle("cardInner");
-}
+// var class1 = "null";
+// var class2 = "null";
 
-// flipCard()
+// function flipCard(id){
+//     (class1 = id) || (class2 = id) ? front:back
+// }
+// // check if cards match
+// function checkMatch(){
+//     if(card1 == null){
+//         card1 = id
+//     }
+//     else if (card2 == null){
+//         card2 = id
+//     }
+//     checkMatch(card1, card2)
+// }
 
-function countMatches(){
-    // when a match is made +1 to count.
-    // need disply for count
-}
+// function activeCard(id, cardFace){
+//     if(card1 == id || card2 == id){
+//         $("#" + id).className = cardFace
+//     } else
+//         $("#" + id).className = cardBack
+// }
+
 
 function timer(){
     // when 1st card flipped, start timer. 
