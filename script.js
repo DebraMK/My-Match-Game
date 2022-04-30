0758dc8745096797ff6728cf92355b21be8b4524
DebraMK <dmkochrn@outlook.com>
2022-04-30 17:16:50 -0500

    The timer works

--- script.js
+++ script.js
@@ -18,15 +18,17 @@ var activeGame = false;
 
 // addEventListener = when player clicks on card, it flips and displays a symbol
 
-
 function flipCard() {
-    while (!activeGame) {
-        console.log ("hi");
-        activeGame == true;
+
+    if (!activeGame) {
+        activeGame = true;
+        countTimer();
     }
     if (freezeState) return;
+
     this.classList.toggle('flip');
-    if(!flippedCard){
+
+    if (!flippedCard) {
         flippedCard = true;
         cardOne = this;
     } 
@@ -59,10 +61,10 @@ function checkMatch(){
 }
 
 // shuffle cards
-function shuffle(){
+function shuffle() {
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 23);
-        card.style.order=randomPos;
+        card.style.order = randomPos;
     })
 }
 
@@ -71,39 +73,45 @@ cards.forEach(card => card.addEventListener("click", flipCard));
 // when 1st card flipped, start timer. 
 // stop timer when all cards matched.
 
-function gameOver(){
-    if(numberPairs == 0){
+function gameOver() {
+    if (numberPairs == 0) {
         activeGame = false;
      }
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 // ANOTHER TIMER TO TRY
 // var timerVar = setInterval(countTimer, 1000);
 var totalSeconds = 0;
 async function countTimer() {
-    if(activeGame == true) {
-           ++totalSeconds;
-           var hour = Math.floor(totalSeconds /3600);
-           var minute = Math.floor((totalSeconds - hour*3600)/60);
-           var second = totalSeconds - (hour*3600 + minute*60);
-           if(hour < 10)
-             hour = "0"+hour;
-           if(minute < 10)
-             minute = "0"+minute;
-           if(second < 10)
-             second = "0"+second;
-           document.getElementsByClassName("timeDisplay").innerHTML = hour + ":" + minute + ":" + second;
-            await sleep (1000);
-            console.log (totalSeconds);
+    while(activeGame == true) {
+        ++totalSeconds;
+        var hour = Math.floor(totalSeconds / 3600);
+        var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+        var second = totalSeconds - (hour * 3600 + minute * 60);
+        if(hour < 10) {
+            hour = "0" + hour;
         }
+        if(minute < 10) {
+            minute = "0" + minute;
+        }
+        if(second < 10) {
+            second = "0" + second;
+        }
+        document.getElementById("timeDisplay").textContent = hour + " : " + minute + " : " + second;
+        await sleep(1000);
     }
+}
+
 // reset game or new game button
-function refreshPage(){
+function refreshPage() {
     window.location.reload();
 }
 
+shuffle();
 
-shuffle()
 
 
